2953a1ee329c72e296ddb455c74d33a488562179
MaurizioAru <ginopc@tiscali.it>
2017-04-26 19:10:36 +0200

    add route message

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ var express = require('express');
 var app = express();
 var PORT = process.env.port || 8080;
 
+app.get('/', function(req, res, next) {
+	res.send('URL-shortener microservice - by M. Aru');
+});
+
 app.listen(PORT, function(){
 	console.log('Server running on port ' + PORT);
 	console.log('[CTRL+C to exit]');
