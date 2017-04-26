40b81ee1fd1da3bb836b5cea19f1d2fd4f7b50f3
MaurizioAru <ginopc@tiscali.it>
2017-04-26 19:06:34 +0200

    first service version

---
+++ server.js
@@ -0,0 +1,15 @@
+/**
+ *   URL Shortener microservice
+ *
+ *   author  : Maurizio Aru
+ *   created : 2017.04.26
+ */
+
+var express = require('express');
+var app = express();
+var PORT = process.env.port || 8080;
+
+app.listen(PORT, function(){
+	console.log('Server running on port ' + PORT);
+	console.log('[CTRL+C to exit]');
+});
